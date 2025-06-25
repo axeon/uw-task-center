fa8951abeef1841b43bc07ab39676723f3c25822
zhangjin <23231269@qq.com>
2025-06-25 22:31:40 +0800

    build(profile): 更新开发环境配置并调整 Swagger 配置
    -将 Swagger 配置的激活环境从 "default", "test", "dev" 修改为仅 "dev"
    - 重命名 bootstrap-dev.yml为 bootstrap-debug.yml
    - 在 discovery 配置中添加 register-enabled: false

--- src/main/java/uw/task/center/conf/SwaggerConfig.java
+++ src/main/java/uw/task/center/conf/SwaggerConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile({"default", "test", "dev"})
+@Profile({"dev"})
 public class SwaggerConfig {
 
     /**
